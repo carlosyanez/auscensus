423b3dfd0afee9ac3711dd3e110299fd273fafff
carlosyanez <carlos.yanez.s@gmail.com>
2022-12-21 00:02:16 +1100

    Update onLoad.R

--- R/onLoad.R
+++ R/onLoad.R
@@ -5,8 +5,10 @@
   home       <- Sys.getenv("HOME")
   cache_path <- file.path(home, ".auscensus_cache")
 
-  if(!dir.exists(cache_path))
+  if(!dir.exists(cache_path)){
     manage_cache_dir(cache_path)
+    Sys.setenv(auscensus_cache_dir=cache_path)
+  }
 
   remove_census_cache_csv()
 
