6ed6a5cebe0fc36764d0b2f6120081f2ed98451e
carlosyanez <carlos.yanez.s@gmail.com>
2023-01-15 18:14:48 +1100

    modified tests for github actions

--- tests/testthat/test-get_content.R
+++ tests/testthat/test-get_content.R
@@ -1,9 +1,16 @@
 test_that("get data", {
 
+  data_2021 <- data_census_info() |>
+               dplyr::select(dplyr::any_of("path")) |>
+               dplyr::pull()
 
+  data_2021 <- any(stringr::str_detect(data_2021,"2021.zip"))
+
+  ## Tests won't run without data, so they will be skip if there is no data
+
+  if(data_2021){
   get_census_data(census_table = list_census_tables("04"),
                   geo_structure = "LGA")
-
 
   data <- get_census_data(census_table = list_census_tables("04"),
                           geo_structure = "LGA",
@@ -31,5 +38,6 @@ test_that("get data", {
                      geo_unit_names  = c("Melbourne","Stonnington","Yarra"),
                      reference_total = list("Total"=c("Total_persons")),
                      geo_structure = "LGA")
+  }
 
 })
